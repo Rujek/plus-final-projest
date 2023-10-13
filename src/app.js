f298dbdbe4277d2d7fb4ac120fc82e71e662f6a0
Sharon Mutyakureva <122147606+Rujek@users.noreply.github.com>
2023-10-13 16:42:41 +0200

    Called forecast API

---
+++ src/app.js
@@ -0,0 +1,148 @@
+function currentTime(timestamp) {
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    if (hours < 10) {
+      hours = `0${hours}`;
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    let day = days[date.getDay()];
+    return `${day} ${hours}: ${minutes}`;
+  }
+  
+  function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let day = date.getDay();
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  
+    return days[day];
+  }
+  // Inject HTML to display the weekly forecast
+  function displayForecast(response) {
+    let dailyForecast = response.data.daily;
+    let forecastElement = document.querySelector("#forecast");
+  
+    let forecastHTML = `<div class="row">`;
+    dailyForecast.forEach(function (dailyForecastDay, index) {
+      if (index < 6) {
+        forecastHTML =
+          forecastHTML +
+          `<div class="col-2 forecast-day">
+          <div class="weekly-forecast-date"> ${formatDay(
+            dailyForecastDay.dt
+          )}</div>
+              <img
+                src="http://openweathermap.org/img/wn/${
+                  dailyForecastDay.weather[0].icon
+                }@2x.png"
+                alt=""
+                width="60"
+              />
+              <div class= "weather-range">
+                <span class="maximum-temp">${Math.round(
+                  dailyForecastDay.temp.max
+                )}°</span><span class="minimum-temp">${Math.round(
+            dailyForecastDay.temp.min
+          )}°</span>
+               </div>
+       </div>`;
+      }
+    });
+  
+    forecastHTML = forecastHTML + `</div>`;
+    forecastElement.innerHTML = forecastHTML;
+  }
+  
+  // call forecast API
+  function getForecast(coordinates) {
+    let apiKey = "9eca7aac0b071aa16e3cb063adba0785";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayForecast);
+  }
+  function displayWeather(response) {
+    let temperatureElement = document.querySelector("#current-temp");
+    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  
+    let descriptionElement = document.querySelector("#description");
+    descriptionElement.innerHTML = response.data.weather[0].description;
+  
+    let maximumTempElement = document.querySelector("#max-temperature");
+    maximumTempElement.innerHTML = Math.round(response.data.main.temp_max);
+  
+    let minimumTempElement = document.querySelector("#min-temperature");
+    minimumTempElement.innerHTML = Math.round(response.data.main.temp_min);
+  
+    let windElement = document.querySelector("#wind");
+    windElement.innerHTML = Math.round(response.data.wind.speed);
+  
+    let humidElement = document.querySelector("#humidity");
+    humidElement.innerHTML = Math.round(response.data.main.humidity);
+  
+    let feelsLikeElement = document.querySelector("#feels-like");
+    feelsLikeElement.innerHTML = Math.round(response.data.main.feels_like);
+  
+    let timeElement = document.querySelector("#current-time");
+    timeElement.innerHTML = currentTime(response.data.dt * 1000);
+  
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = response.data.name;
+  
+    let iconElement = document.querySelector("#icon");
+    iconElement.setAttribute(
+      "src",
+      `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
+    icon.setAttribute("alt", response.data.weather[0].description);
+  
+    celsiusTemperature = response.data.main.temp;
+  
+    getForecast(response.data.coord);
+  }
+  function search(city) {
+    let apiKey = "9eca7aac0b071aa16e3cb063adba0785";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayWeather);
+  }
+  
+  function handleSubmit(event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#input-city");
+    search(cityInputElement.value);
+  }
+  
+  function displayFahrenheitTemp(event) {
+    event.preventDefault();
+    let temperatureElement = document.querySelector("#current-temp");
+    let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+    temperatureElement.innerHTML = `${Math.round(fahrenheitTemperature)}°F`;
+  }
+  
+  let celsiusTemperature = null;
+  
+  function displayCelsiusTemp(event) {
+    event.preventDefault();
+    let temperatureElement = document.querySelector("#current-temp");
+    temperatureElement.innerHTML = `${Math.round(celsiusTemperature)}°C`;
+  }
+  
+  let celsiusLink = document.querySelector("#celsius-link");
+  celsiusLink.addEventListener("click", displayCelsiusTemp);
+  
+  let fahrenheitLink = document.querySelector("#fahrenheit-link");
+  fahrenheitLink.addEventListener("click", displayFahrenheitTemp);
+  
+  let form = document.querySelector("#search-form");
+  form.addEventListener("submit", handleSubmit);
+  
+  search("Johannesburg");
